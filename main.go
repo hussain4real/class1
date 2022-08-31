cf5b4cf822dafb94b896e23601fdd4ff9780f125
Hussain Aminu <hussain_aminu@yahoo.com>
2022-08-31 18:41:06 +0100

    learned slices

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func printMyGithubName(githubName string) {
-	fmt.Print(githubName)
+	fmt.Println(githubName)
 }
 
 func main() {
-	printMyGithubName("hussain4real")
+	printMyGithubName("hussain4real\n")
+
+	var months []string
+
+	/*var songName = []string{
+		"song1",
+		"song2",
+		"song3",
+	}*/
+
+	months = []string{
+		"jan",
+		"feb",
+		"mar",
+		"apr",
+		"may",
+		"jun",
+		"jul",
+		"aug",
+		"sep",
+		"oct",
+		"nov",
+		"dec",
+	}
+	allMonth := months[4:]
+	fmt.Printf("this is all the month: %v", allMonth)
 }
